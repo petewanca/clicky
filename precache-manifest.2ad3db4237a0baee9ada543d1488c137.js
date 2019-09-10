c1fe13596524f4a97c5c8efbdb1888c8b709b68b
pete <petewanca@gmail.com>
2019-09-10 15:00:05 -0500

    Updates

---
+++ precache-manifest.2ad3db4237a0baee9ada543d1488c137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201af16d0926c60d5fd5db58a4f3fc02",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "d2547c6794911de3830a",
+    "url": "/clicky/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "0810fb170a0c3464d7db",
+    "url": "/clicky/static/js/2.eb3ceb77.chunk.js"
+  },
+  {
+    "revision": "d2547c6794911de3830a",
+    "url": "/clicky/static/js/main.9a2d64b2.chunk.js"
+  },
+  {
+    "revision": "9d378f700a36f547cf37",
+    "url": "/clicky/static/js/runtime~main.68383fad.js"
+  }
+]);
